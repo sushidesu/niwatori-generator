019becf47c7165d69010ca92f94723c2b76dc0b1
sushidesu <hugemerge@gmail.com>
2020-09-12 12:09:40 +0000

    Add firebase config

---
+++ src/plugins/Firebase.ts
@@ -0,0 +1,17 @@
+import firebase from "firebase/app"
+import "firebase/storage"
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_DATABASEURL,
+  projectId: process.env.NEXT_PUBLIC_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_APPID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENTID,
+}
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+
+export const storage = firebaseApp.storage()
