84e6e294aed4f23c2cb540bea9033036e27c0569
sushidesu <hugemerge@gmail.com>
2020-09-12 05:07:34 +0000

    Update to input text

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
-import React, { useRef }  from "react"
+import React, { useState, useRef }  from "react"
 import domToImage from "dom-to-image"
 
 const Home = () => {
   const previewRef = useRef<HTMLDivElement>(null)
+  const [text, setText] = useState("にわとり")
 
   const generate = async () => {
     const dataUrl = await domToImage.toPng(previewRef.current)
@@ -17,9 +18,11 @@ const Home = () => {
       <h1>庭には2羽ニワトリがいるジェネレーター</h1>
       <div style={{ width: "200px", height: "200px", backgroundColor: "white" }} ref={previewRef}>
         <p>this is preview</p>
-        <p><b>niwatori</b></p>
+        <p><b>{ text }</b></p>
         <p style={{ color: "red" }}>niwatori</p>
       </div>
+      <hr />
+      <input value={text} onChange={e => setText(e.target.value)} />
       <button onClick={generate}>start</button>
     </div>
   )
